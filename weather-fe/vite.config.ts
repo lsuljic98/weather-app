b336972fb9426c4cfa57688d4a97458c7c2e9fb1
Leon Suljić <leon.suljic@olympus.com>
2026-08-27 15:48:49 +0200

    feat: add initial fe solution

---
+++ weather-fe/vite.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    proxy: { '/api': { target: 'http://api:8080', changeOrigin: true } }
+  }
+})
